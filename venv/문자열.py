f27dfa94eb8dcfda078cc24f6c91e5a12a2e75a4
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-04-02 11:58:37 +0900

    문자열 자료형 (인덱싱)

---
+++ venv/문자열.py
@@ -0,0 +1,16 @@
+# sentence = "Python's favorite food"
+# sentence1 = 'he said, "Today is Saturday!"'
+#
+#
+# print(sentence)
+# print(sentence1)
+#
+# head="python"
+# tail=" is fun"
+#
+# print(head + tail)
+# print(head * 5)
+# print(len(tail))
+
+# string = "홀짝홀짝홀짝"
+# print(string[0]+string[2]+string[4])
---
+++ venv/숫자형.py
@@ -0,0 +1,6 @@
+x = int(input("정수를 입력하세요: "))
+
+min = x // 60
+sec = x % 60
+
+print(min, "분", sec, "초")
