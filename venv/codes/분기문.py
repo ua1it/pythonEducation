c0e21708a974a0995d97db9b7868833bef37eea2
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-06-18 20:42:08 +0900

    분기문 연습 추가

---
+++ venv/codes/분기문.py
@@ -0,0 +1,15 @@
+#1
+#2
+def 분기문2_1():
+    A = int(input("입력값: "))
+    A += 20
+    if A > 255:
+        print("출력값:",255)
+    else:
+        print("출력값:",A)
+#3
+# x는 참, y는 거짓
+# x and y -> x 그리고 y
+# x or y -> x 또는 y
+# not x -> x가 아니다
+# not y -> y가 아니다
